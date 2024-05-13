58577dfe6cca1c0485d38e874735c9febc68912a
cotruongsg <cotruongsg@gmail.com>
2024-05-12 22:30:27 -0700

    Implement Cypress & AppliTools & Github Action coverage & Jest for React testing app

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testConcurrency: 1,
+    apiKey: '0zpKekTSgKliZajh4yfa6B7dM9kQutZx9OnA2MGbdN0110',
+    batchName: 'TAU Todo App',
+    browser: [
+        {width: 800, height: 600, name: 'chrome'},
+        {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+    ]
+}
